36f64fcab6771d007e70a88e859472db7a44e349
Juanan Ruiz <juanan@us.es>
2024-11-06 22:39:08 +0100

    Templates funcionando para el CRUD de convocatorias

--- src/Controller/ConvocatoriaController.php
+++ src/Controller/ConvocatoriaController.php
@@ -14,68 +14,68 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/convocatoria')]
 class ConvocatoriaController extends AbstractController
 {
-    #[Route('/', name: 'convocatoria_index', methods: ['GET'])]
-    public function index(ConvocatoriaRepository $convocatoriaRepository): Response
-    {
-        return $this->render('convocatoria/index.html.twig', [
-            'convocatorias' => $convocatoriaRepository->findAll(),
-        ]);
-    }
+	#[Route('/', name: 'convocatoria_index', methods: ['GET'])]
+	public function index(ConvocatoriaRepository $convocatoriaRepository): Response
+	{
+		return $this->render('convocatoria/index.html.twig', [
+			'convocatorias' => $convocatoriaRepository->findAll(),
+		]);
+	}
 
-    #[Route('/new', name: 'convocatoria_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $convocatoria = new Convocatoria();
-        $form = $this->createForm(ConvocatoriaType::class, $convocatoria);
-        $form->handleRequest($request);
+	#[Route('/new', name: 'convocatoria_new', methods: ['GET', 'POST'])]
+	public function new(Request $request, EntityManagerInterface $entityManager): Response
+	{
+		$convocatoria = new Convocatoria();
+		$form = $this->createForm(ConvocatoriaType::class, $convocatoria);
+		$form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($convocatoria);
-            $entityManager->flush();
+		if ($form->isSubmitted() && $form->isValid()) {
+			$entityManager->persist($convocatoria);
+			$entityManager->flush();
 
-            return $this->redirectToRoute('convocatoria_index');
-        }
+			return $this->redirectToRoute('convocatoria_index');
+		}
 
-        return $this->render('convocatoria/new.html.twig', [
-            'convocatoria' => $convocatoria,
-            'form' => $form,
-        ]);
-    }
+		return $this->render('convocatoria/new.html.twig', [
+			'convocatoria' => $convocatoria,
+			'form' => $form,
+		]);
+	}
 
-    #[Route('/{id}', name: 'convocatoria_show', methods: ['GET'])]
-    public function show(Convocatoria $convocatoria): Response
-    {
-        return $this->render('convocatoria/show.html.twig', [
-            'convocatoria' => $convocatoria,
-        ]);
-    }
+	#[Route('/{id}/show', name: 'convocatoria_show', methods: ['GET'])]
+	public function show(Convocatoria $convocatoria): Response
+	{
+		return $this->render('convocatoria/show.html.twig', [
+			'convocatoria' => $convocatoria,
+		]);
+	}
 
-    #[Route('/{id}/edit', name: 'convocatoria_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Convocatoria $convocatoria, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(ConvocatoriaType::class, $convocatoria);
-        $form->handleRequest($request);
+	#[Route('/{id}/edit', name: 'convocatoria_edit', methods: ['GET', 'POST'])]
+	public function edit(Request $request, Convocatoria $convocatoria, EntityManagerInterface $entityManager): Response
+	{
+		$form = $this->createForm(ConvocatoriaType::class, $convocatoria);
+		$form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
+		if ($form->isSubmitted() && $form->isValid()) {
+			$entityManager->flush();
 
-            return $this->redirectToRoute('convocatoria_index');
-        }
+			return $this->redirectToRoute('convocatoria_index');
+		}
 
-        return $this->render('convocatoria/edit.html.twig', [
-            'convocatoria' => $convocatoria,
-            'form' => $form,
-        ]);
-    }
+		return $this->render('convocatoria/edit.html.twig', [
+			'convocatoria' => $convocatoria,
+			'form' => $form,
+		]);
+	}
 
-    #[Route('/{id}', name: 'convocatoria_delete', methods: ['POST'])]
-    public function delete(Request $request, Convocatoria $convocatoria, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete' . $convocatoria->getId(), $request->request->get('_token'))) {
-            $entityManager->remove($convocatoria);
-            $entityManager->flush();
-        }
+	#[Route('/{id}/delete', name: 'convocatoria_delete', methods: ['POST'])]
+	public function delete(Request $request, Convocatoria $convocatoria, EntityManagerInterface $entityManager): Response
+	{
+		if ($this->isCsrfTokenValid('delete' . $convocatoria->getId(), $request->request->get('_token'))) {
+			$entityManager->remove($convocatoria);
+			$entityManager->flush();
+		}
 
-        return $this->redirectToRoute('convocatoria_index');
-    }
+		return $this->redirectToRoute('convocatoria_index');
+	}
 }
