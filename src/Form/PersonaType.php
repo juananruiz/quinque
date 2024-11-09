fe24bcbd279a96639c31d425151b3a99039e71a9
Juanan Ruiz <juanan@us.es>
2024-11-09 19:36:17 +0100

    Añade la posibilidad de elegir departamento para una persona

--- src/Form/PersonaType.php
+++ src/Form/PersonaType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Persona;
+use App\Entity\Departamento;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,12 +17,26 @@ class PersonaType extends AbstractType
             ->add('nombre')
             ->add('apellidos')
             ->add('telefono')
-            ->add('fechaNacimiento', null, ['widget' => 'single_text', ])
-            ->add('dni');
+            ->add('fechaNacimiento', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('dni')
+            ->add('departamento', EntityType::class, [
+                'class' => Departamento::class,
+                'choice_label' => 'nombre',
+                'placeholder' => 'Seleccione un departamento',
+                'required' => true,
+                'label' => 'Departamento',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults(['data_class' => Persona::class,]);
+        $resolver->setDefaults([
+            'data_class' => Persona::class,
+        ]);
     }
 }
