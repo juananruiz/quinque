de74c0c25eecb8d4fea651c0eac33df61afc0537
Juanan Ruiz <juanan@us.es>
2024-12-24 19:09:38 +0100

    Adaptando controladores al estilo Sirhus

--- src/Controller/Quinque/SolicitudEstadoController.php
+++ src/Controller/Quinque/SolicitudEstadoController.php
@@ -5,7 +5,7 @@ namespace App\Controller\Quinque;
 use App\Entity\Quinque\SolicitudEstado;
 use App\Form\Quinque\SolicitudEstadoType;
 use App\Repository\Quinque\SolicitudEstadoRepository;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Service\MessageGenerator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,16 +14,11 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('intranet/quinque/admin/solicitud_estado', name: 'intranet_quinque_admin_solicitud_estado_')]
 class SolicitudEstadoController extends AbstractController
 {
-    private EntityManagerInterface $entityManager;
-    private SolicitudEstadoRepository $solicitudEstadoRepository;
-
+    
     public function __construct(
-        EntityManagerInterface $entityManager,
-        SolicitudEstadoRepository $solicitudEstadoRepository,
-    ) {
-        $this->entityManager = $entityManager;
-        $this->solicitudEstadoRepository = $solicitudEstadoRepository;
-    }
+        private readonly MessageGenerator $generator,
+        private readonly SolicitudEstadoRepository $solicitudEstadoRepository
+    ) {}
 
     #[Route('/', name: 'index', methods: ['GET'])]
     public function index(): Response
@@ -36,13 +31,16 @@ class SolicitudEstadoController extends AbstractController
     #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
     public function new(Request $request): Response
     {
+        $this->denyAccessUnlessGranted('admin');
         $solicitudEstado = new SolicitudEstado();
         $form = $this->createForm(SolicitudEstadoType::class, $solicitudEstado);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->entityManager->persist($solicitudEstado);
-            $this->entityManager->flush();
+            $this->solicitudEstadoRepository->save($solicitudEstado, true);
+            $this->generator->logAndFlash('info', 'Nuevo estado de solicitud creado', [
+                'id' => $solicitudEstado->getId(),
+            ]);
 
             return $this->redirectToRoute('intranet_quinque_admin_solicitud_estado_index');
         }
@@ -56,6 +54,8 @@ class SolicitudEstadoController extends AbstractController
     #[Route('/show/{id}', name: 'show', methods: ['GET'])]
     public function show(SolicitudEstado $solicitudEstado): Response
     {
+        $this->denyAccessUnlessGranted('admin');
+        
         return $this->render('intranet/quinque/admin/solicitud_estado/show.html.twig', [
             'solicitud_estado' => $solicitudEstado,
         ]);
@@ -64,11 +64,15 @@ class SolicitudEstadoController extends AbstractController
     #[Route('/edit/{id}', name: 'edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, SolicitudEstado $solicitudEstado): Response
     {
+        $this->denyAccessUnlessGranted('admin');
         $form = $this->createForm(SolicitudEstadoType::class, $solicitudEstado);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->entityManager->flush();
+            $this->solicitudEstadoRepository->save($solicitudEstado, true);
+            $this->generator->logAndFlash('info', 'Estado de solicitud actualizado', [
+                'id' => $solicitudEstado->getId(),
+            ]);
 
             return $this->redirectToRoute('intranet_quinque_admin_solicitud_estado_index');
         }
@@ -82,9 +86,10 @@ class SolicitudEstadoController extends AbstractController
     #[Route('/delete/{id}', name: 'delete', methods: ['POST'])]
     public function delete(Request $request, SolicitudEstado $solicitudEstado): Response
     {
+        $this->denyAccessUnlessGranted('admin');
         if ($this->isCsrfTokenValid('delete'.$solicitudEstado->getId(), $request->request->get('_token'))) {
-            $this->entityManager->remove($solicitudEstado);
-            $this->entityManager->flush();
+            $this->solicitudEstadoRepository->remove($solicitudEstado, true);
+            $this->generator->logAndFlash('info', 'Estado de solicitud eliminado');
         }
 
         return $this->redirectToRoute('intranet_quinque_admin_solicitud_estado_index');
